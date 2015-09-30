83774c26fa50480eb701d7563b959de62d181a4e
Bernhard Tittelbach <bernhard@tittelbach.org>
2015-09-30 20:37:46 +0200

    SetOverlayState

--- dtslots.go
+++ dtslots.go
@@ -8,6 +8,31 @@ import (
 	"regexp"
 )
 
+var foundit_error_ error
+
+func init() {
+	foundit_error_ = fmt.Errorf("Success")
+}
+
+func makeFindDirHelperFunc(returnvalue *string, path_base string, target_re, interm_re *regexp.Regexp) func(string, os.FileInfo, error) error {
+	return func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			return nil
+		}
+		if path == path_base {
+			return nil
+		}
+		if target_re.MatchString(path) {
+			*returnvalue = path
+			return foundit_error_ //foundit
+		}
+		if interm_re != nil && !interm_re.MatchString(path) {
+			return filepath.SkipDir //skipdir if not like path_re1
+		}
+		return nil //continue walking
+	}
+}
+
 func findSlotsFile() (sfile string, err error) {
 	foundit := fmt.Errorf("Success")
 	path_base := "/sys/devices"
@@ -32,6 +57,42 @@ func findSlotsFile() (sfile string, err error) {
 		err = nil
 	}
 	sfile = tdir + "/slots"
+	return
+}
+
+func findOverlayStateFile(dtb_name string) (sfile string, err error) {
+	foundit := fmt.Errorf("Success")
+	path_base := "/sys/devices"
+	path_re1 := "^" + path_base + "/ocp" + `\.\d+`
+	path_re2 := path_re1 + "/" + dtb_name + `\.\d+`
+	re1 := regexp.MustCompile(path_re1 + "$")
+	re2, err := regexp.Compile(path_re2)
+	if err != nil {
+		return
+	}
+	findOverlayStateFileBBB := func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			return nil
+		}
+		if path == path_base {
+			return nil
+		}
+		if re2.MatchString(path) {
+			sfile = path
+			return foundit //foundit
+		}
+		if !re1.MatchString(path) {
+			return filepath.SkipDir //skipdir if not like path_re1
+		}
+		return nil //continue walking
+	}
+	err = filepath.Walk(path_base, findOverlayStateFileBBB)
+	if err == foundit {
+		err = nil
+	} else {
+		err = fmt.Errorf("NotFound")
+	}
+	sfile = sfile + "/state"
 	return
 }
 
@@ -80,4 +141,29 @@ func RemoveDeviceTreeOverlay(dtb_name string) (err error) {
 		}
 	}
 	return fmt.Errorf("DeviceTreeOverlay %s not found in slots\n", dtb_name)
+}
+
+// Overlays like PyBBIO-gpio.* can, once loaded, be configured using file /sys/devices/ocp.\d/PyBBIO-gpio.*.\d\d/state
+// usually the following values can be written to configure the state:
+// "mode_0b00101111" => INPUT, No Pullup/down
+// "mode_0b00110111" => INPUT, Pullup
+// "mode_0b00100111" => INPUT, Pulldown
+// "mode_0b00001111" => OUTPUT, No Pullup/down
+// "mode_0b00010111" => OUTPUT, Pullup
+// "mode_0b00000111" => OUTPUT, Pulldown
+func SetOverlayState(dtb_name, state string) (err error) {
+	var overlaystatefile string
+	var statefh *os.File
+	overlaystatefile, err = findOverlayStateFile(dtb_name)
+	if err != nil {
+		return err
+	}
+	statefh, err = os.OpenFile(overlaystatefile, os.O_WRONLY|os.O_SYNC, 0666)
+	if err != nil {
+		return
+	}
+	defer statefh.Close()
+	statefh.Truncate(0)
+	statefh.WriteString(state)
+	return
 }
